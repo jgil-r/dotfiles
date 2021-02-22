22e68a51fd960dd047990a2d6f5c5c66495c1bd2
Jesus Gil <jesusgil.dev@gmail.com>
2021-02-21 21:11:59 -0600

    add coc-settings

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -3,13 +3,22 @@ local extensions = require('el.extensions')
 local sections = require('el.sections')
 local subscribe = require('el.subscribe')
 
+local git_icon = subscribe.buf_autocmd("el_file_icon", "BufRead", function(_, bufnr)
+  local icon = extensions.file_icon(_, bufnr)
+  if icon then
+    return icon .. ' '
+  end
+
+  return ''
+end)
+
 local git_branch = subscribe.buf_autocmd(
   "el_git_branch",
   "BufEnter",
   function(window, buffer)
     local branch = extensions.git_branch(window, buffer)
     if branch then
-      return ' ' .. branch
+      return ' ' .. extensions.git_icon() .. ' ' .. branch
     end
   end
 )
@@ -23,6 +32,7 @@ require('el').setup {
       git_branch,
       ' ',
       sections.split,
+      git_icon,
       builtin.tail_file,
       sections.split,
       builtin.line, '/', builtin.number_of_lines,  ':', builtin.column, ' ',
